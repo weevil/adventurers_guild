149b614110e696dc7da5ab199b73c698a4ac433b
Wiley Wiggins <weevil@wileywiggins.com>
2017-12-02 15:32:10 -0600

    added pyglet, created a window. Still needs some text formatting and refreshing the character on keypress doesn’t work yet.

--- guild.py
+++ guild.py
@@ -3,12 +3,45 @@
 #import modules
 import random
 import character
+import pyglet
+from pyglet.window import key
 
-def main():
-    print("Adventurer's Guild")
-    seed= random.randint(0,9999)
-    newCharacter = character.Character()
-    newCharacter.generateCharacter(seed)
-    print(newCharacter)
-    
-main()
+newCharacter = character.Character()
+window = pyglet.window.Window(500, 800, caption='Adventurer\'s Guild')
+document = pyglet.text.document.FormattedDocument()
+layout = pyglet.text.layout.TextLayout(document, 500, 800)
+title = pyglet.text.Label('Press ENTER to summon an entity',
+                          font_name='Fredoka One',
+                          font_size=16,
+                          color=(249,104,107,255),
+                          x=window.width//2, y=window.height-16,
+                          anchor_x='center', anchor_y='center')
+
+characterText = pyglet.text.Label( str(newCharacter),
+                                    font_name='ProFontIIx',
+                                    font_size=10,
+                                    color=(15,10,10,255),
+                                    x=10,y=window.height - 40,
+                                    anchor_x='left', anchor_y='top')
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print('A key was pressed')
+    if symbol == key.ENTER:
+        seed= random.randint(0,9999)
+        newCharacter.generateCharacter(seed)
+        print('Enter was pressed')
+    return newCharacter
+    window.push_handlers(on_key_press)
+
+@window.event
+def on_draw():
+    pyglet.gl.glClearColor(240,240,240,255)
+    window.clear()
+    title.draw()
+    characterText.draw()
+
+
+
+
+pyglet.app.run()
